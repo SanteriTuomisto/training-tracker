1bdd03d211d4aa2d6519b5a809e3f9e7c5996a26
Santeri Tuomisto <tuomisto.santeri.m@gmail.com>
2019-07-05 12:19:52 +0300

    Exercise list sorting

--- src/components/exercises/ExerciseList.js
+++ src/components/exercises/ExerciseList.js
@@ -6,29 +6,39 @@ import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import Badge from 'react-bootstrap/Badge'
 
-
-
 class ExerciseList extends React.Component {
+  state = { selectedCategories: [] }; 
+
   // todo hae kannasta
   // todo uusien lisäys
-  // todo lihaksen kuva?
-  categories = [
-    { id: 0, title: 'Chest' },
-    { id: 1, title: 'Bicep' },
-    { id: 2, title: 'Back' }
-  ];
-
+  // todo kuva?
   exercises = [
     { id: 0, title: 'Bench press', categoria: 'Chest', description: "Rintaliikkeiden kuningas" },
     { id: 1, title: 'Squat', categoria: 'Legs', description: "Jalkaliikkeiden kuningas" },
-    { id: 2, title: 'Deadlift', categoria: 'Back', description: "Koko kropan kuningas" }
+    { id: 2, title: 'Deadlift', categoria: 'Back', description: "Koko kropan kuningas" },
+    { id: 3, title: 'Pushdown', categoria: 'Triceps', description: "Vemputus" }
   ];
 
-  renderCategoriesList(categories) {
-    return categories.map(categoria => {
+  selectCategoria(title) {
+    if (this.state.selectedCategories.includes(title)) {
+      const filtered = this.state.selectedCategories.filter(item => item !== title);
+      this.setState({ selectedCategories: filtered });
+    }
+    else {
+      this.setState(prevState => ({
+        selectedCategories: [...prevState.selectedCategories, title]
+      }));
+    }
+  }
+
+  renderCategoriesList(exercises) {
+    return exercises.map(exercise => {
       return (
-        <Button variant="secondary" key={categoria.id}>
-          {categoria.title}
+        <Button onClick={() => this.selectCategoria(exercise.categoria)} 
+          variant={(this.state.selectedCategories.includes(exercise.categoria) ? 'primary' : 'secondary')} 
+          key={exercise.id}
+        >
+          {exercise.categoria}
         </Button>
       );
     });
@@ -36,17 +46,22 @@ class ExerciseList extends React.Component {
 
   renderExerciseList(exercises) {
     return exercises.map(exercise => {
-      return (
-        <Col sm={6} key={exercise.id}>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>{exercise.title} <Badge variant="secondary">{exercise.categoria}</Badge></Card.Title>
-              <Card.Text>{exercise.description}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      );
+      if (this.state.selectedCategories.length === 0 || this.state.selectedCategories.includes(exercise.categoria)) {
+        return (
+          <Col sm={6} key={exercise.id}>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>{exercise.title} <Badge variant="secondary">{exercise.categoria}</Badge></Card.Title>
+                <Card.Text>{exercise.description}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      }
+      else {
+        return null;
+      }
     });
   }
 
@@ -62,7 +77,7 @@ class ExerciseList extends React.Component {
           <Col sm={8}>
             <h4>Sort exercises by:</h4>
             <ButtonGroup aria-label="Basic example">
-              {this.renderCategoriesList(this.categories)}
+              {this.renderCategoriesList(this.exercises)}
             </ButtonGroup>
             <h4>Exercises:</h4>
             <Row>
